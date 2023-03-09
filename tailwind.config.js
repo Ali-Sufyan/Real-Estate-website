0b92114a46ff98056f44d5315a078c050ab17c82
Ali-Sufyan <alisufyanofficial0037@gmail.com>
2023-03-09 23:44:07 +0500

    update code

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      "boxShadow":{
+        "btn": "0px 10px 15px rgba(1, 115, 153, 0.2)"
+      },
+      fontSize:{
+        sm:'0.75rem', // 12px
+        base:'0.875rem',//14px
+        md:'1rem', //16px
+        lg:'1.25rem', //20px
+        xl:'1.5rem', //24px
+        "2xl":'1.75rem', //28px
+        "3xl":'2.25rem', //36px
+        "4xl":'3rem',
+        "5xl":'4rem',
+        // "ali":'10rem',
+      }
+    },
+  },
+  plugins: [],
+}
